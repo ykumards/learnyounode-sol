7d7b8988d6bb626fd2bd4063ac3add75d91cc96a
ykumards <ykumar@nyu.edu>
2016-08-22 11:25:38 -0400

    added post uppercaser

---
+++ 12_http_uppercaser.js
@@ -0,0 +1,13 @@
+var map = require('through2-map')
+var http = require('http')
+
+var port = process.argv[2]
+
+var server = http.createServer(function(req, res){
+  if (req.method != 'POST')
+    return res.end("accepts only POST")
+  req.pipe(map(function(chunk){
+    return chunk.toString().toUpperCase()
+  })).pipe(res)
+})
+server.listen(port)
